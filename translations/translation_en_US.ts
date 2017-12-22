52eeede49f61aa8e09444b5758dc3188f4796ec0
Renguin0829 <ryjh8118@gmail.com>
2017-12-22 13:25:58 +0800

    12/22 ***可以將值傳到PHP

--- translations/translation_en_US.ts
+++ translations/translation_en_US.ts
@@ -12,10 +12,9 @@
     <context>
         <name>behavior_1/behavior.xar:/Diagram/Say</name>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Hello</source>
             <comment>Text</comment>
-            <translation>Hello</translation>
+            <translation type="vanished">Hello</translation>
         </message>
     </context>
     <context>
--- translations/translation_zh_CN.ts
+++ translations/translation_zh_CN.ts
@@ -12,10 +12,9 @@
     <context>
         <name>behavior_1/behavior.xar:/Diagram/Say</name>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Hello</source>
             <comment>Text</comment>
-            <translation>你好</translation>
+            <translation type="vanished">你好</translation>
         </message>
     </context>
     <context>
